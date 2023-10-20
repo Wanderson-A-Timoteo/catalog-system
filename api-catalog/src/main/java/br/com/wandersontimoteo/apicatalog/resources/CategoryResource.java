369e21bbb64ec6dc832639fca21c3f50b13ac605
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-10-20 18:34:32 -0400

    feat: add insert Category

--- api-catalog/src/main/java/br/com/wandersontimoteo/apicatalog/resources/CategoryResource.java
+++ api-catalog/src/main/java/br/com/wandersontimoteo/apicatalog/resources/CategoryResource.java
@@ -4,11 +4,10 @@ import br.com.wandersontimoteo.apicatalog.dto.CategoryDTO;
 import br.com.wandersontimoteo.apicatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +27,16 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO categoryDTO = categoryService.findById(id);
         return ResponseEntity.ok().body(categoryDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = categoryService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
