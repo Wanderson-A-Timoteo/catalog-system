e8edd485d2d9c4c4ec76a78bcf9879201dd74851
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-10-23 19:58:28 -0400

    feat: add Product insert, update and delete

--- api-catalog/src/main/java/br/com/wandersontimoteo/apicatalog/resources/ProductResource.java
+++ api-catalog/src/main/java/br/com/wandersontimoteo/apicatalog/resources/ProductResource.java
@@ -8,6 +8,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/produtos")
@@ -33,6 +36,28 @@ public class ProductResource {
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO productDTO = productService.findById(id);
         return ResponseEntity.ok().body(productDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
+        productDTO = productService.insert(productDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productDTO.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(productDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        productDTO = productService.update(id, productDTO);
+        return ResponseEntity.ok().body(productDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- api-catalog/src/main/java/br/com/wandersontimoteo/apicatalog/services/ProductService.java
+++ api-catalog/src/main/java/br/com/wandersontimoteo/apicatalog/services/ProductService.java
@@ -1,15 +1,22 @@
 package br.com.wandersontimoteo.apicatalog.services;
 
+import br.com.wandersontimoteo.apicatalog.dto.CategoryDTO;
 import br.com.wandersontimoteo.apicatalog.dto.ProductDTO;
+import br.com.wandersontimoteo.apicatalog.entities.Category;
 import br.com.wandersontimoteo.apicatalog.entities.Product;
+import br.com.wandersontimoteo.apicatalog.repositories.CategoryRepository;
 import br.com.wandersontimoteo.apicatalog.repositories.ProductRepository;
+import br.com.wandersontimoteo.apicatalog.services.exceptions.DatabaseException;
 import br.com.wandersontimoteo.apicatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -17,6 +24,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -30,6 +40,52 @@ public class ProductService {
         Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Produto com id: " + id +
                 ", não encontrada"));
         return new ProductDTO(entity, entity.getCategories());
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO productDTO) {
+        Product entity = new Product();
+        copyDtoToEntity(productDTO, entity);
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO productDTO) {
+        try {
+            Product entity = productRepository.getOne(id);
+            copyDtoToEntity(productDTO, entity);
+            entity = productRepository.save(entity);
+            return new ProductDTO(entity);
+        } catch (EntityNotFoundException error) {
+            throw new ResourceNotFoundException("Produto com id: " + id + ", não encontrado para atualizar");
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            productRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException error) {
+            throw new ResourceNotFoundException("Produto com id: " + id + ", não encontrado para excluir");
+        } catch (DataIntegrityViolationException err) {
+            throw new DatabaseException("Não é possível excluir o produto de id: "+ id +
+                    ", pois existe uma ou mais categorias cadastradas no banco de dados"+
+                    " com este produto");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO productDTO, Product entity) {
+        entity.setName(productDTO.getName());
+        entity.setDescription(productDTO.getDescription());
+        entity.setDate(productDTO.getDate());
+        entity.setPrice(productDTO.getPrice());
+        entity.setImgUrl(productDTO.getImgUrl());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : productDTO.getCategories()) {
+            Category category = categoryRepository.getOne(catDto.getId());
+            entity.getCategories().add(category);
+        }
     }
 
 }
