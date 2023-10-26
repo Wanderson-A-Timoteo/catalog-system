50316043dff3d0a035ee9a1d702ba4bafe07feb9
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-10-26 18:45:05 -0400

    test: Created ProductServiceTests and configured simulated behavior with Mockito in @BeforeEach

---
+++ api-catalog/src/test/java/br/com/wandersontimoteo/apicatalog/services/ProductServiceTests.java
@@ -0,0 +1,37 @@
+package br.com.wandersontimoteo.apicatalog.services;
+
+import br.com.wandersontimoteo.apicatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.doNothing().when(productRepository).deleteById(existingId);
+        Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(nonExistingId);
+
+    }
+
+}
