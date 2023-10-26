77c34bdd14bb81cdde3c1db5079c99a6ccb4853a
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-10-26 19:04:23 -0400

    test: Method Delete in ProductService - delete Should Throw Resource Not Found Exception When Id Does Not Exist

--- api-catalog/src/test/java/br/com/wandersontimoteo/apicatalog/services/ProductServiceTests.java
+++ api-catalog/src/test/java/br/com/wandersontimoteo/apicatalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package br.com.wandersontimoteo.apicatalog.services;
 
 import br.com.wandersontimoteo.apicatalog.repositories.ProductRepository;
+import br.com.wandersontimoteo.apicatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,7 +24,6 @@ public class ProductServiceTests {
 
     private long existingId;
     private long nonExistingId;
-    private long countTotalProducts;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -31,16 +32,27 @@ public class ProductServiceTests {
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
         Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(nonExistingId);
-
     }
 
     @Test
     public void deleteShouldDoNothingWhenIdExists() {
+
         Assertions.assertDoesNotThrow(() -> {
             productService.delete(existingId);
         });
 
         Mockito.verify(productRepository, Mockito.times(1)).deleteById(existingId);
+
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.delete(nonExistingId);
+        });
+
+        Mockito.verify(productRepository, Mockito.times(1)).deleteById(nonExistingId);
 
     }
 
